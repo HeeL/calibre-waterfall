d4b5da52fbe517ba69196cca54deddfeb7ba3713
Sergii Paryzhskyi <sergii.paryzhskyi@holidaycheck.com>
2019-06-01 14:02:13 +0200

    Move server index to its own directory

--- server/index.js
+++ server/index.js
@@ -2,7 +2,9 @@ const express = require("express");
 const next = require("next");
 
 const port = 3000;
-const app = next({ dev: true });
+const port = process.env.PORT || 3000;
+const dev = process.env.NODE_ENV !== "production";
+const app = next({ dev });
 const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
