3cd815e90391d99cbdd54f7cc9a74e40b098a451
Sergii Paryzhskyi <sergii.paryzhskyi@holidaycheck.com>
2019-06-04 01:17:59 +0200

    Fetch sites from /api/sites and list them on the page

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,26 @@
 import React from "react";
 import Link from "next/link";
+import fetch from "isomorphic-unfetch";
 
-export default () => (
+const Index = props => (
   <div>
-    <Link href="/sites/cruises">Cruises</Link>
+    {props.sites.map(site => (
+      <Link key={site.slug}>
+        <a href={`/sites/${site.slug}`}>{site.name}</a>
+        <br />
+      </Link>
+    ))}
   </div>
 );
+
+Index.getInitialProps = ({ req }) => {
+  const baseUrl = req ? `${req.protocol}://${req.get("Host")}` : "";
+
+  return fetch(`${baseUrl}/api/sites`)
+    .then(result => result.json())
+    .then(sites => ({
+      sites
+    }));
+};
+
+export default Index;
--- test/unit/pages/indexSpec.js
+++ test/unit/pages/indexSpec.js
@@ -1,22 +1,21 @@
 import { shallow } from "enzyme";
 import React from "react";
 import { expect } from "chai";
-import Link from "next/link";
 
 import Index from "../../../pages/index.js";
 
 describe("Index Page", () => {
   it("renders the page", () => {
-    const index = shallow(<Index />);
+    const index = shallow(<Index sites={[]} />);
 
     expect(index).to.be.present();
   });
 
   it("renders links to sites", () => {
-    const index = shallow(<Index />);
-    const links = index.find(Link);
+    const index = shallow(<Index sites={[{ slug: "test", name: "bar" }]} />);
+    const links = index.find("Link a");
 
     expect(links).to.have.length(1);
-    expect(links.first()).to.have.prop("href", "/sites/cruises");
+    expect(links.first()).to.have.prop("href", "/sites/test");
   });
 });
