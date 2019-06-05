256d9ef6916a6989bc09e167e88cb5fc67828cfa
Sergii Paryzhskyi <sergii.paryzhskyi@holidaycheck.com>
2019-06-06 00:13:56 +0200

    Add page  for listing snapshots of a site

--- pages/sites.js
+++ pages/sites.js
@@ -1,5 +1,27 @@
 import React from "react";
+import Link from "next/link";
 
-const Sites = props => <div>Page Sites</div>;
+const Sites = props => (
+  <div>
+    {props.snapshots.map(snapshot => (
+      <Link key={snapshot.id} href={`/snapshots/${snapshot.id}`}>
+        <a>
+          #{snapshot.id} {snapshot.created_at}
+        </a>
+      </Link>
+    ))}
+  </div>
+);
+
+Sites.getInitialProps = ({ req, query: { slug } }) => {
+  console.log(slug);
+  const baseUrl = req ? `${req.protocol}://${req.get("Host")}` : "";
+
+  return fetch(`${baseUrl}/api/snapshots`)
+    .then(result => result.json())
+    .then(snapshots => ({
+      snapshots
+    }));
+};
 
 export default Sites;
---
+++ test/unit/pages/sitesSpec.js
@@ -0,0 +1,30 @@
+import { shallow } from "enzyme";
+import React from "react";
+import { expect } from "chai";
+import Link from "next/link";
+
+import Sites from "../../../pages/sites.js";
+
+describe("Sites Page", () => {
+  it("renders the page", () => {
+    const sites = shallow(<Sites snapshots={[]} />);
+
+    expect(sites).to.be.present();
+  });
+
+  it("renders links to snapshots", () => {
+    const sites = shallow(
+      <Sites
+        snapshots={[
+          { id: 1, created_at: "2019-06-05T05:03:45.236Z" },
+          { id: 42, created_at: "2018-06-05T05:03:45.236Z" }
+        ]}
+      />
+    );
+    const links = sites.find(Link);
+
+    expect(links).to.have.length(2);
+    expect(links.first()).to.have.prop("href", "/snapshots/1");
+    expect(links.at(1)).to.have.prop("href", "/snapshots/42");
+  });
+});
