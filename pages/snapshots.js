dafb17cc9ff02ef738c12fbe684adbb38c29b241
Sergii Paryzhskyi <sergii.paryzhskyi@holidaycheck.com>
2019-07-07 02:05:20 +0200

    Display waterfall on snapshot details page

---
+++ pages/snapshots.js
@@ -0,0 +1,39 @@
+import React from "react";
+import fetch from "isomorphic-unfetch";
+import "react-perfcascade/dist/react-perfcascade.css";
+
+class Snapshots extends React.Component {
+  constructor() {
+    super();
+    this.state = { client: false };
+  }
+
+  componentDidMount() {
+    this.setState({ client: true });
+  }
+
+  renderHar() {
+    const HarFileView = require("react-perfcascade").HarFileView;
+    return <HarFileView harData={this.props.harData} />;
+  }
+
+  render() {
+    return <div>{this.state.client ? this.renderHar() : null}</div>;
+  }
+}
+
+Snapshots.getInitialProps = ({ req, query: { id, slug } }) => {
+  const baseUrl = req ? `${req.protocol}://${req.get("Host")}` : "";
+
+  return fetch(`${baseUrl}/api/har/${slug}/${id}/1`)
+    .then(response => response.json())
+    .then(response => {
+      return fetch(response.harUrl)
+        .then(result => result.json())
+        .then(harData => {
+          return { harData };
+        });
+    });
+};
+
+export default Snapshots;
--- routes.js
+++ routes.js
@@ -2,3 +2,4 @@ const nextRoutes = require("next-routes");
 const routes = (module.exports = nextRoutes());
 
 routes.add("sites", "/sites/:slug");
+routes.add("snapshots", "/sites/:slug/snapshots/:id");
